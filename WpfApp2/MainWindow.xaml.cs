31dbee5f82f1e9214cceee66464315abfbfdef6c
Ahme_cn21 <Ahme_cn21@STHQ0122-09>
2022-09-06 21:45:18 +0400

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AIMLbot;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Bot AI = new Bot();
+            AI.loadSettings();
+            AI.loadAIMLFromFiles();
+            AI.isAcceptingUserInput = false;
+            User user = new User("Username here", AI);
+            AI.isAcceptingUserInput = true;
+            Request request = new Request(myTxtBox.Text, user, AI);
+            Result result = AI.Chat(request);
+            myLabel.Content = result.Output;
+        }
+    }
+}
